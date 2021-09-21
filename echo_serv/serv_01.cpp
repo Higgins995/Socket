0821bf2ee390fe3e81f6302397755a2093405463
h <higgins995@qq.com>
2021-09-21 20:26:09 +0800

    echo server

--- echo_serv/serv_01.cpp
+++ echo_serv/serv_01.cpp
@@ -1,44 +1,64 @@
-#include <sys/socket.h>
-#include <stdio.h>
+// 单连接 回声服务器
+// higgins995
+// 2021/09/21
+// 20:17
 #include <stdlib.h>
-#include <unistd.h>
-#include <arpa/inet.h>
+#include <stdio.h>
 #include <string.h>
+#include <unistd.h>
+#include <sys/socket.h>
+#include <arpa/inet.h>
+
 #define BUF_SIZE 1024
 
 void error_handling(char *message);
 
-int mian(int argc, char *argv[]) {
-	int sock;
-	char []message = "Hello, World!";
+int main(int argc, char *argv[]) {
+	int serv_sock, clnt_sock;
+	struct sockaddr_in serv_addr, clnt_addr;
+	socklen_t clnt_addr_sz;
 	int str_len;
-	struct sockaddr_in serv_addr;
+	char message[BUF_SIZE];
 
-	if (argc != 3) {
-		printf("Usage : %s <IP> <port>\n", argv[0]);
+	if (argc != 2) {
+		printf("Usage : %s <port>\n", argv[0]);
 		exit(1);
 	}
 
 	serv_sock = socket(PF_INET, SOCK_STREAM, 0);
-	if (sock == -1) error_handling("socket() error!");
-	
+	if (serv_sock == -1) error_handling("socket() error!");
+
 	memset(&serv_addr, 0, sizeof(serv_addr));
 	serv_addr.sin_family = AF_INET;
-	serv_addr.sin_addr.s_addr = inet(argv[1]);
-	serv_addr.sin_port = htons(atoi(argv[2]));
+	serv_addr.sin_addr.s_addr = htonl(INADDR_ANY);
+	serv_addr.sin_port  = htons(atoi(argv[1]));
 
-	if (connect(sock, (struct sockaddr*) &serv_add, sizeof(serv_add)) == -1)
-		error_handling("connect() error!");
-	else
-		puts("Connected.........");
+	if (bind(serv_sock, (struct sockaddr*)&serv_addr, sizeof(serv_addr)) == -1)
+		error_handling("bind() error!");
 
+	if (listen(serv_sock, 5) == -1) error_handling("listen() error!");
+
+	clnt_addr_sz = sizeof(clnt_addr);
+	clnt_sock = accept(serv_sock, (struct sockaddr*)&clnt_addr, &clnt_addr_sz);
+
+	if (clnt_sock == -1) error_handling("accept() error!");
+
+	while (1) {
+		str_len = read(clnt_sock, message, BUF_SIZE);
+		if (str_len == 0) break;
+		if (message == "\n" | message == " ") break;
+		printf("Message from clinet : %s", message);
+
+		write(clnt_sock, message, str_len);
+	}
 	
-	close(sock);
+	close(clnt_sock);
+	close(serv_sock);
 	return 0;
 }
 
 void error_handling(char *message) {
 	fputs(message, stderr);
-	fputc('\n'. stderr);
+	fputc('\n', stderr);
 	exit(1);
 }
